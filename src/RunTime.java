d7854d7ef72836cfcbfd799bb4a05fc162939b7c
tobias landgraf <tola1502@gmail.com>
2021-01-11 16:41:17 +0100

    Ein wunderschönen Kommentar

--- src/RunTime.java
+++ src/RunTime.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class RunTime {
 
     public static void main(String[] args) {
-        System.out.println("Hallo");
 
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Hallo Herzlich Willkommen auf der Titanic 2");
+
+        // number of people
+        System.out.println("Mit wie vielen Personen, möchten Sie an Bord gehen?");
+        int people = scanner.nextInt();
+
+
+        // duration of the journey
+        System.out.println("Für wie viele Tage duren wir sie begrüßen?");
+        int stay = scanner.nextInt();
+
+
+        // type of cabin (in/outside)
+        System.out.println("Möchten sie eine Außenkabine?");
+        String typeOfCabin = scanner.nextLine();
+
+
+
+        System.out.println("Die Titanic 2 wünscht eine sichchere Reise - Gemeinsam gehen wir nicht wieder unter :)");
+
+        scanner.close();
     }
 }
 
