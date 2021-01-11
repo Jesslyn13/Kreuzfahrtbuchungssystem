dc2c1816835ffbfc93d828e54f3946589706ac04
Jesslyn13 <jo.grossmann@arcor.de>
2021-01-11 15:26:50 +0100

    Adjusted Gitignore

--- src/Cabin.java
+++ src/Cabin.java
@@ -1,9 +1,32 @@
 public class Cabin {
 
-    static final int MAX_GUEST_COUNT = 4;
+    static final int PRESIDENT_SUITE = 5;
 
     boolean booked;
-    int size;
     boolean hasWindow;
+    int size;
+    int costPerDay;
+
+    public Cabin(boolean hasWindow, int size) {
+        this.hasWindow=hasWindow;
+        this.size=size;
+        calculateCost();
+
+    }
+
+    public void setBookedStatus(boolean isBooked) {
+                booked=isBooked;
+    }
+
+    public void calculateCost() {
+        switch (size) {
+            case 1: costPerDay=250; break;
+            case 2: costPerDay=200; break;
+            case 3: costPerDay=175; break;
+            case 4: costPerDay=150; break;
+            case PRESIDENT_SUITE: costPerDay=15000; break;
+        }
+        if(hasWindow) costPerDay*=1.4;
+    }
 
 }
--- src/RunTime.java
+++ src/RunTime.java
@@ -1,6 +1,8 @@
 public class RunTime {
+
     public static void main(String[] args) {
         System.out.println("Hallo");
+
     }
 }
 
