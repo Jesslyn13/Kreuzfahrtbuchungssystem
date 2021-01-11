bb72ca9350e47b85d579e670796f1cd79cdb99ac
Jesslyn13 <jo.grossmann@arcor.de>
2021-01-11 16:06:29 +0100

    Update Cabin.java

--- src/Cabin.java
+++ src/Cabin.java
@@ -2,14 +2,20 @@ import java.util.Map;
 
 public class Cabin {
 
-    static final int PRESIDENT_SUITE = 5;
+    //President suite is assigned a "size" so it can simply be a part of the price map
+
+    static final int PRESIDENT_SUITE_KEY = 5;
+    static final double OUTSIDE_CABIN_COST_MULTIPLIER = 1.4;
+
+    // This map sets up the size of cabins to correspond to a specific price
+    // President Suite can not be easily described like this, so it is set as a constant
+
     static final Map<Integer,Integer> PRICE_PER_DAY = Map.of(
             1,250,
             2,200,
             3,175,
             4,150,
-            PRESIDENT_SUITE,15000);
-
+            PRESIDENT_SUITE_KEY,15000);
     // variables
     boolean booked;
     boolean hasWindow;
@@ -23,16 +29,16 @@ public class Cabin {
         calculateCost();
     }
 
-    // booked status
     public void setBookedStatus(boolean isBooked) {
         booked=isBooked;
     }
 
-    // Cabin costs
     public void calculateCost() {
-        costPerDay=PRICE_PER_DAY.get(size);
-        if(hasWindow) costPerDay*=1.4;
+        costPerDay=PRICE_PER_DAY.get(size); // default cost for a cabin simply depend on its size
 
+        if(hasWindow) {
+            costPerDay *= OUTSIDE_CABIN_COST_MULTIPLIER;   // cabins with a window are more expensive by a fixed multiplier
+        }
 
     }
 
