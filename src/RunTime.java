c831f029530b7663c15bcbeb24fe0a60ee56e0d0
tobias landgraf <tola1502@gmail.com>
2021-01-11 17:59:28 +0100

    RunTime Update

--- src/RunTime.java
+++ src/RunTime.java
@@ -6,16 +6,15 @@ public class RunTime {
 
         Scanner scanner = new Scanner(System.in);
 
-
         System.out.println("Hallo Herzlich Willkommen auf der Titanic 2");
 
         // number of people
-        System.out.println("Mit wie vielen Personen, möchten Sie an Bord gehen?");
+        System.out.println("Mit wie vielen Personen möchten Sie an Bord gehen?");
         int people = scanner.nextInt();
 
 
         // duration of the journey
-        System.out.println("Für wie viele Tage duren wir sie begrüßen?");
+        System.out.println("Für wie viele Tage dürfen wir sie begrüßen?");
         int stay = scanner.nextInt();
 
 
@@ -24,10 +23,12 @@ public class RunTime {
         String typeOfCabin = scanner.nextLine();
 
 
+        // calculation of the total cost
+        System.out.println("Ihr " + stay + " tägier Aufenthalt bei uns kostet insgesammt " + TotalCost + " €.");
+        System.out.println("Sie haben Zimmernummer: " + RomeNumber);
 
         System.out.println("Die Titanic 2 wünscht eine sichchere Reise - Gemeinsam gehen wir nicht wieder unter :)");
 
         scanner.close();
     }
 }
-
