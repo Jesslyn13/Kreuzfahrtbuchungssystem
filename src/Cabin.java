4a0799973beeedcf2a40e6c79f578311107552ad
Jesslyn13 <jo.grossmann@arcor.de>
2021-01-12 08:36:06 +0100

    Update Cabin.java

--- src/Cabin.java
+++ src/Cabin.java
@@ -5,10 +5,10 @@ public class Cabin {
 
     static final double OUTSIDE_CABIN_COST_MULTIPLIER = 1.4;
 
-    //President suite is assigned a "size" so it can simply be a part of the price map
+    // President suite is assigned a "size" so it can simply be a part of the price map
     static final int PRESIDENT_SUITE_KEY = 5;
 
-    // Setting up a map so the size of cabins to correspond to a specific price
+    // Setting up a map so the size of cabins correspond to a specific price
     static final Map<Integer,Integer> PRICE_PER_DAY = Map.of(
             1,250,
             2,200,
@@ -16,23 +16,26 @@ public class Cabin {
             4,150,
             PRESIDENT_SUITE_KEY,15000);
 
-    boolean booked;
     boolean hasWindow;
-    int    size;
+    int guestsBooked;
+    int size;
     int costPerDay;
 
-    public Cabin(boolean hasWindow, int size) {
+    public Cabin(boolean hasWindow, int size, int guestCount) {
+        this.guestsBooked=guestCount;
         this.hasWindow=hasWindow;
         this.size=size;
         calculateCost();
     }
 
-    public void setBookedStatus(boolean isBooked) {
-        booked=isBooked;
+
+    public void setBookedStatus(int guestsBooked) {
+        guestsBooked = this.guestsBooked;
     }
 
     public void calculateCost() {
         costPerDay = PRICE_PER_DAY.get(size); // default cost for a cabin simply depend on its size
+        costPerDay *= guestsBooked;
 
         if(hasWindow) {
             costPerDay *= OUTSIDE_CABIN_COST_MULTIPLIER;   // cabins with a window are more expensive by a fixed multiplier
