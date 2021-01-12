818300e18c97280901d0cf595ca2dc4c6bc35214
tobias landgraf <tola1502@gmail.com>
2021-01-12 08:57:33 +0100

    Ein wunderschönen Kommentar

--- src/RunTime.java
+++ src/RunTime.java
@@ -1,31 +1,41 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class RunTime {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner scanner = new Scanner(System.in);
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = dateFormat.parse("10.01.2021");
+
 
         System.out.println("Hallo Herzlich Willkommen auf der Titanic 2");
 
         // number of people
-        System.out.println("Mit wie vielen Personen möchten Sie an Bord gehen?");
-        int people = scanner.nextInt();
+      //  System.out.println("Wie viele Personen sind?");
+       // int people = scanner.nextInt();
 
 
         // duration of the journey
-        System.out.println("Für wie viele Tage dürfen wir sie begrüßen?");
-        int stay = scanner.nextInt();
+       // System.out.println("An welchem Datum möchten sie an Bord kommen?");
+       // int startDateOfStay = scanner.nextInt();
+
+       // System.out.println("An welchem Datum gehen sie von Bord?");
+       // int endDateOfStay = scanner.nextInt();
 
 
         // type of cabin (in/outside)
-        System.out.println("Möchten sie eine Außenkabine?");
-        String typeOfCabin = scanner.nextLine();
+     //   System.out.println("Möchten sie eine Außenkabine?");
+      //  String typeOfCabin = scanner.nextLine();
 
 
         // calculation of the total cost
-        System.out.println("Ihr " + stay + " tägier Aufenthalt bei uns kostet insgesammt " + TotalCost + " €.");
-        System.out.println("Sie haben Zimmernummer: " + RomeNumber);
+       // System.out.println("Ihr Aufenthalt vom " + startDateOfStay + " bis zum " + endDateOfStay + "  bei uns kostet insgesammt €.");
 
         System.out.println("Die Titanic 2 wünscht eine sichchere Reise - Gemeinsam gehen wir nicht wieder unter :)");
 
