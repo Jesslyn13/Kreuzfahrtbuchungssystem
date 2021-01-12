46be5540603b0b3f2448de3e4c19d561cfa14d02
Jesslyn13 <jo.grossmann@arcor.de>
2021-01-12 10:49:32 +0100

    Adjusted Gitignore

--- src/DataInput.java
+++ src/DataInput.java
@@ -10,21 +10,15 @@ public class DataInput {
 
     Boolean wantWindow;
 
-    // Entering the guest data
     public void CheckIn() throws ParseException {
 
         System.out.println("Hallo Herzlich Willkommen auf der Titanic 2");
-
-        // number of people
         System.out.println("Mit wie vielen Persoen kommen sie an Bord?");
         int people = scanner.nextInt();
 
-
-        // duration of the journey
         System.out.println("Wie viele Tage soll ihre Reise dauern?");
         int duritationOfStay = scanner.nextInt();
 
-        // type of cabin (in/outside)
         System.out.println("Möchten sie eine Außenkabine? ");
         String typeOfCabin = scanner.next();
         if (typeOfCabin.equalsIgnoreCase("Ja")) {
@@ -33,10 +27,7 @@ public class DataInput {
             wantWindow = false;
         }
 
-
-        // calculation of the total cost
         System.out.println("Ihr Aufenthalt bei uns kostet insgesammt €.");
-
         System.out.println("Die Titanic 2 wünscht eine sichchere Reise - Gemeinsam gehen wir nicht wieder unter :)");
 
         scanner.close();
