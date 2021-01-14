f292dd95d841d0c79c62cbe256b4fc0c2390aba8
Jesslyn13 <jo.grossmann@arcor.de>
2021-01-14 16:00:06 +0100

    removed TODO list in runtime

--- src/RunTime.java
+++ src/RunTime.java
@@ -3,17 +3,7 @@ import java.text.ParseException;
 public class RunTime {
 
     public static void main(String[] args) throws ParseException {
-
         ShipLogic SL = new ShipLogic();
         new DataInput().CheckIn(SL);
     }
 }
-
-/*
-
- - Filter/Suchfunktionen etc
- - (GUI)
- - Code Kommentieren
- - Fehlersuche
-
- */
